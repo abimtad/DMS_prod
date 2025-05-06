619a5907c25fbf99873a294844ff0a386a74b715
Abel Tadesse <tadesseabel26@gmail.com>
2025-05-06 18:30:00 +0300

    fix router import

--- client/src/components/error-state.tsx
+++ client/src/components/error-state.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { RefreshCw, Rocket, AlertTriangle } from "lucide-react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function ErrorState() {
