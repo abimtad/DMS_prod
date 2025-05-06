dbe8c5edcd32cdd9755cec5e6862c2db47befa73
Abel Tadesse <tadesseabel26@gmail.com>
2025-05-07 01:38:55 +0300

    fix: enforce secure cookies and set SameSite attribute in token generation

--- server/src/lib/generateToken.ts
+++ server/src/lib/generateToken.ts
@@ -13,6 +13,7 @@ export const generateTokenAndSetCookie = async (
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
+    sameSite: "none",
   });
 };
